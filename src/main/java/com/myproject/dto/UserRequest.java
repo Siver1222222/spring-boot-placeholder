db5021ca24419a2e90b35fa857787ff8f37dda30
siver sharif <siversharif@gmail.com>
2025-04-23 11:00:00 +0200

    Added UserRequest DTO class for user input validation

---
+++ src/main/java/com/myproject/dto/UserRequest.java
@@ -0,0 +1,52 @@
+package com.example.socialhub.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+
+public class UserRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    // Constructor
+    public UserRequest() {
+    }
+
+    public UserRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
